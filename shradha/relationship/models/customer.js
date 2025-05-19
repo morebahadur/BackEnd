42bbaf6e6d1f94f328345d7637ed513f765d371a
morebahadur <dt1414926@gmail.com>
2025-05-19 09:20:52 +0545

    file update

--- shradha/relationship/models/customer.js
+++ shradha/relationship/models/customer.js
@@ -22,20 +22,28 @@ const customerSchema = new Schema({
     ],
 });
 
+
 const Order = mongoose.model("Order", orderSchema);
 const Customer = mongoose.model("Customer", customerSchema);
-const addCustomer = async () => {
-    let cust1 = new Customer({
-        name: "rahul kumar",
-    });
-    let order1 = await Order.findOne({ items: "chips" });
-    let order2 = await Order.findOne({ items: "aalu" });
-    cust1.orders.push(order1);
-    cust1.orders.push(order2);
-    let result = await cust1.save();
-    console.log(result);
-}
-addCustomer();
+
+customerSchema.post("findOneAndDelete", async function (customer) {
+    if (customer.orders.length) {
+        let rst = await Order.deleteMany({ _id: { $in: customer.orders } });
+        console.log(rst);
+    }
+});
+// const addCustomer = async () => {
+//     let cust1 = new Customer({
+//         name: "rahul kumar",
+//     });
+//     let order1 = await Order.findOne({ items: "chips" });
+//     let order2 = await Order.findOne({ items: "aalu" });
+//     cust1.orders.push(order1);
+//     cust1.orders.push(order2);
+//     let result = await cust1.save();
+//     console.log(result);
+// }
+// addCustomer();
 // const addUsers = async () => {
 //     let result = await Order.insertMany([
 //         { items: "samosa", price: 123 }, 
@@ -44,4 +52,24 @@ addCustomer();
 //     ]);
 //     console.log(result);
 // }
+const newCustomer = async () => {
+    let cust1 = new Customer({
+        name: "aran arjun",
+    });
 
+    let order1 = new Order({
+        items: "Pizza",
+        price: 123
+    });
+
+    cust1.orders.push(order1);
+    await order1.save();
+    await cust1.save();
+    console.log("new customer added!");
+}
+const deleteCustomer = async () => {
+    let data = await Customer.findByIdAndDelete('68292c09cd1e9fef70476277');
+    console.log(data);
+}
+// newCustomer();
+deleteCustomer();
